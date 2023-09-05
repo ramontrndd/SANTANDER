4fa0db39104288fc5f67008adba76e2cd79a2140
ramontrndd <134494678+ramontrndd@users.noreply.github.com>
2023-09-05 12:47:02 -0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,12 +2,12 @@ const pokemonList = document.getElementById('pokemonList')
 const loadMoreButton = document.getElementById('loadMoreButton')
 
 const maxRecords = 151
-const limit = 10
+const limit = 8;
 let offset = 0;
 
 function convertPokemonToLi(pokemon) {
     return `
-        <li class="pokemon ${pokemon.type}">
+       <li class="pokemon ${pokemon.type}">
             <span class="number">#${pokemon.number}</span>
             <span class="name">${pokemon.name}</span>
 
